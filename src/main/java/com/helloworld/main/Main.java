2c1c3349a7e67302c41bd79937129b2a6ed09c9a
Bohdan Potapskyi <bohdan.potapsky@gmail.com>
2019-08-13 16:33:42 +0300

    commit 1

--- src/main/java/com/helloworld/main/Main.java
+++ src/main/java/com/helloworld/main/Main.java
@@ -3,6 +3,6 @@ package com.helloworld.main;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world2");
+        System.out.println("Hello world3");
     }
 }
